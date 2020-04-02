e5d08855637d29d56168629e0ccf54b102a7e005
huyfr <61008601+huyfr@users.noreply.github.com>
2020-04-02 23:07:28 +0700

    Triển khai stack sử dụng lớp LinkedList trong thư viện java.uti

---
+++ src/GenericStackClient.java
@@ -0,0 +1,8 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integer");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("1. Stack of String");
+        MyGenericStack.stackOfStrings();
+    }
+}
---
+++ src/MyGenericStack.java
@@ -0,0 +1,62 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public static void stackOfStrings() {
+        MyGenericStack<String> stack = new MyGenericStack<>();
+        stack.push("five");
+        stack.push("four");
+        stack.push("three");
+        stack.push("two");
+        stack.push("one");
+        System.out.println("1.1 Size of stack after push operations: " + stack.size());
+        System.out.print("1.2 Pop elements from stack: ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %s", stack.pop());
+        }
+        System.out.println("\n1.3 Size of stack after pop operations: " + stack.size());
+    }
+
+    public static void stackOfIntegers() {
+        MyGenericStack<Integer> stack = new MyGenericStack<>();
+        stack.push(5);
+        stack.push(4);
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        System.out.println("2.1. Size of stack after push operations: " + stack.size());
+        System.out.print("2.2. Pop elements from stack : ");
+        while (!stack.isEmpty()) {
+            System.out.printf(" %d", stack.pop());
+        }
+        System.out.println("\n3.3 Size of stack after pop operations : " + stack.size());
+    }
+}
